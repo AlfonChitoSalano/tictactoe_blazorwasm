dbed45112779d1c0ae94240f8f3724dd50f50c9f
Chito Salano <Alfonchitosalao2@gmail.com>
2022-10-28 18:32:13 +0800

    added 1

--- TicTacToe.Business/Entities/Player.cs
+++ TicTacToe.Business/Entities/Player.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TicTacToe.Business.Entities
+﻿namespace TicTacToe.Business.Entities
 {
     public class Player
     {
